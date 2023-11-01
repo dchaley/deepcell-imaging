9b17c196b82f9b3301ba8c2a430ba3f58b6b5bb8
David Haley <dchaley@gmail.com>
2023-11-01 15:53:04 -0700

    Implement reconstruct algorithm in normal python
    As expected, this is much slower than the scikit and opencv methods,
    both of which use C code for the heavy inner loops.
    
    This needs to be tested on more input images: I'm a bit skeptical of the
    queue portion as the code worked with or without the queue processing
    for the tested sample… (DeepCell mesmer sample #3)
    
    Benchmarking:
    
    ```
    Image size: (512, 512)
    scikit h_maxima: 0.15 s
    opencv h_maxima: 0.01 s
    OpenCV Speedup: 9.75x
    scikit == opencv : True
    custom python h_maxima: 9.62 s
    Python speedup: 0.02x
    scikit == custom python : True
    ```

--- benchmarking/scikit-opencv/benchmark_utils.py
+++ benchmarking/scikit-opencv/benchmark_utils.py
@@ -1,6 +1,7 @@
+from collections import deque
 import cv2
+import logging
 import numpy as np
-import sys
 from skimage.morphology import h_maxima
 from skimage.morphology import disk
 import timeit
@@ -37,6 +38,205 @@ def opencv_reconstruct(marker: np.ndarray, mask: np.ndarray, radius: int = 2):
         np.copyto(dst=marker, src=expanded)
 
 
+def reconstruct_fast_hybrid_python(
+    marker: np.ndarray, mask: np.ndarray, radius: int = 2
+):
+    # N(G), the pixels in our neighborhood.
+    footprint = disk(radius)
+
+    footprint_rows = footprint.shape[0]
+    footprint_center_row = footprint_rows // 2
+    footprint_cols = footprint.shape[1]
+    footprint_center_col = footprint_cols // 2
+
+    # N+(G), the pixels *before* in a raster scan.
+    # This is all footprint points before the center.
+    ones_before = np.concatenate(
+        [
+            np.ones((footprint_rows * footprint_cols) // 2 + 1, dtype=bool),
+            np.zeros((footprint_rows * footprint_cols) // 2, dtype=bool),
+        ]
+    ).reshape((footprint_rows, footprint_cols))
+    footprint_raster_before = footprint * ones_before
+    # N-(G), the pixels *after* in a raster scan.
+    ones_after = np.concatenate(
+        [
+            np.zeros((footprint_rows * footprint_cols) // 2, dtype=bool),
+            np.ones((footprint_rows * footprint_cols) // 2 + 1, dtype=bool),
+        ]
+    ).reshape((footprint_rows, footprint_cols))
+    footprint_raster_after = footprint * ones_after
+
+    # .item() converts the numpy scalar to a python scalar
+    border_value = np.min(marker).item()
+
+    marker_rows = marker.shape[0]
+    marker_cols = marker.shape[1]
+
+    queue = deque()
+
+    # Scan in raster order.
+    t = timeit.default_timer()
+    for row in range(marker_rows):
+        for col in range(marker_cols):
+            # If the marker is already at the maximum mask value, skip this pixel.
+            if marker[row, col] == mask[row, col]:
+                continue
+
+            # OOB values get the border value
+            neighborhood_max = border_value
+            for footprint_row_offset in range(
+                -footprint_center_row, footprint_center_row + 1
+            ):
+                for footprint_col_offset in range(
+                    -footprint_center_col, footprint_center_col + 1
+                ):
+                    if not footprint_raster_before[
+                        footprint_center_row + footprint_row_offset,
+                        footprint_center_col + footprint_col_offset,
+                    ]:
+                        continue
+
+                    if (
+                        row + footprint_row_offset < 0
+                        or row + footprint_row_offset >= marker_rows
+                        or col + footprint_col_offset < 0
+                        or col + footprint_col_offset >= marker_cols
+                    ):
+                        # The border cannot be greater than the minimum; skip.
+                        continue
+                    else:
+                        pixel_value = marker[
+                            row + footprint_row_offset, col + footprint_col_offset
+                        ]
+                        neighborhood_max = max(neighborhood_max, pixel_value)
+
+            marker[row, col] = min(neighborhood_max, mask[row, col])
+
+    # Scan in reverse-raster order.
+    for row in range(marker_rows - 1, -1, -1):
+        for col in range(marker_cols - 1, -1, -1):
+            neighborhood_max = border_value
+
+            for footprint_row_offset in range(
+                -footprint_center_row, footprint_center_row + 1
+            ):
+                for footprint_col_offset in range(
+                    -footprint_center_col, footprint_center_col + 1
+                ):
+                    # Skip this point if not in the footprint.
+                    footprint_x = footprint_center_row + footprint_row_offset
+                    footprint_y = footprint_center_col + footprint_col_offset
+                    if not footprint_raster_after[footprint_x, footprint_y]:
+                        continue
+
+                    # Skip out of bounds
+                    if (
+                        row + footprint_row_offset < 0
+                        or row + footprint_row_offset >= marker_rows
+                        or col + footprint_col_offset < 0
+                        or col + footprint_col_offset >= marker_cols
+                    ):
+                        continue
+
+                    pixel_value = marker[
+                        row + footprint_row_offset, col + footprint_col_offset
+                    ]
+                    neighborhood_max = max(neighborhood_max, pixel_value)
+
+            marker[row, col] = min(neighborhood_max, mask[row, col])
+
+            enqueue = False
+            for footprint_row_offset in range(
+                -footprint_center_row, footprint_center_row + 1
+            ):
+                for footprint_col_offset in range(
+                    -footprint_center_col, footprint_center_col + 1
+                ):
+                    # Don't consider self as neighbor.
+                    if footprint_row_offset == 0 and footprint_col_offset == 0:
+                        continue
+
+                    # Skip this point if not in the footprint.
+                    footprint_x = footprint_center_row + footprint_row_offset
+                    footprint_y = footprint_center_col + footprint_col_offset
+                    if not footprint_raster_after[footprint_x, footprint_y]:
+                        continue
+
+                    # Skip out of bounds
+                    if (
+                        row + footprint_row_offset < 0
+                        or row + footprint_row_offset >= marker_rows
+                        or col + footprint_col_offset < 0
+                        or col + footprint_col_offset >= marker_cols
+                    ):
+                        continue
+
+                    neighbor_coord = (
+                        row + footprint_row_offset,
+                        col + footprint_col_offset,
+                    )
+
+                    neighbor_value = marker[neighbor_coord]
+                    if (
+                        neighbor_value < marker[row, col]
+                        and neighbor_value < mask[neighbor_coord]
+                    ):
+                        enqueue = True
+                        break
+
+                if enqueue:
+                    break
+
+            if enqueue:
+                queue.append((row, col))
+
+    logging.debug("Raster scan/anti-scan time: %s", timeit.default_timer() - t)
+
+    # Process the queue of pixels that need to be updated.
+    logging.debug("Queue size: %s", len(queue))
+    t = timeit.default_timer()
+    while len(queue) > 0:
+        point = queue.popleft()
+
+        for footprint_row_offset in range(
+            -footprint_center_row, footprint_center_row + 1
+        ):
+            for footprint_col_offset in range(
+                -footprint_center_col, footprint_center_col + 1
+            ):
+                # Don't consider self as neighbor.
+                if footprint_row_offset == 0 and footprint_col_offset == 0:
+                    continue
+
+                if not footprint_raster_after[
+                    footprint_center_row + footprint_row_offset,
+                    footprint_center_col + footprint_col_offset,
+                ]:
+                    continue
+
+                if (
+                    row + footprint_row_offset < 0
+                    or row + footprint_row_offset >= marker_rows
+                    or col + footprint_col_offset < 0
+                    or col + footprint_col_offset >= marker_cols
+                ):
+                    # Skip out of bounds
+                    continue
+
+                neighbor_coord = (
+                    row + footprint_row_offset,
+                    col + footprint_col_offset,
+                )
+                if marker[point] > marker[neighbor_coord] != mask[neighbor_coord]:
+                    marker[neighbor_coord] = min(marker[point], mask[neighbor_coord])
+                    queue.append(neighbor_coord)
+
+    logging.debug("Queue processing time: %s", timeit.default_timer() - t)
+
+    return marker
+
+
 def opencv_h_maxima(image, h=0.075, radius=2):
     # This is mostly copied from scikit h_maxima
     # except using our own grayscale reconstruction
@@ -47,28 +247,11 @@ def opencv_h_maxima(image, h=0.075, radius=2):
     return (residue_img >= h).astype(np.uint8)
 
 
-def main(marker_filename):
-    # Load the data
-    with np.load(marker_filename) as loader:
-        marker_data = np.load(marker_filename)["arr_0"]
-
-    # Run the benchmark
-    t = timeit.default_timer()
-    scikit_result = scikit_h_maxima(marker_data[0, ..., 0])
-    print("scikit h_maxima: %s s" % (timeit.default_timer() - t))
-
-    t = timeit.default_timer()
-    opencv_result = opencv_h_maxima(marker_data[0, ..., 0])
-    print("opencv h_maxima: %s s" % (timeit.default_timer() - t))
-
-    diff = np.abs(scikit_result - opencv_result)
-    print("results sizes: %s, %s" % (scikit_result.shape, opencv_result.shape))
-    print("results     equal: %s" % (diff == 0).sum())
-    print("results different: %s" % (diff != 0).sum())
-    print("smallest difference: %s" % (diff.min()))
-    print(" largest difference: %s" % (diff.max()))
-    print(" average difference: %s" % (diff.mean()))
-
-
-if __name__ == "__main__":
-    main(sys.argv[1])
+def python_h_maxima(image, h=0.075, radius=2):
+    # This is mostly copied from scikit h_maxima
+    # except using our own grayscale reconstruction
+    resolution = 2 * np.finfo(image.dtype).resolution * np.abs(image)
+    shifted_img = image - h - resolution
+    reconstructed = reconstruct_fast_hybrid_python(shifted_img, image, radius)
+    residue_img = image - reconstructed
+    return (residue_img >= h).astype(np.uint8)
