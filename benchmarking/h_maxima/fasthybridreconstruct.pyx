c8639b315bd403bf283d947ff5b040f88aeab3d5
David Haley <dchaley@gmail.com>
2023-11-06 16:51:56 -0800

    Add documentation & clarify naming for fast-hybrid reconstruction

--- benchmarking/h_maxima/fasthybridreconstruct.pyx
+++ benchmarking/h_maxima/fasthybridreconstruct.pyx
@@ -9,6 +9,8 @@ from skimage.morphology import disk
 cimport cython
 from libc.stdint cimport uint8_t
 
+# This fast-hybrid reconstruction algorithm supports the following data types.
+# Adding more should be a simple matter of adding them to this list.
 ctypedef fused my_type:
     int
     float
@@ -22,10 +24,27 @@ cdef my_type get_neighborhood_max(
     Py_ssize_t point_row,
     Py_ssize_t point_col,
     uint8_t[:, ::1] footprint,
-    Py_ssize_t footprint_center_row,
-    Py_ssize_t footprint_center_col,
     my_type border_value,
 ):
+    """Get the neighborhood maximum around a point.
+
+    The neighborhood is defined by a footprint. Points are excluded if
+    the footprint is 0, and included otherwise. The footprint must have
+    an odd number of rows and columns, and is anchored at the center.
+
+    border_value is used for out-of-bounds points. In expected usage,
+    this is the minimum image value.
+
+    Args:
+      image (mytype[][]): the image to scan
+      point_row (Py_ssize_t): the row of the point to scan
+      point_col (Py_ssize_t): the column of the point to scan
+      footprint (uint8_t[][]): the neighborhood footprint
+      border_value (my_type): the value to use for out-of-bound points
+
+    Returns:
+        my_type: the maximum in the point's neighborhood, greater than or equal to border_value.
+    """
     cdef my_type pixel_value
     # OOB values get the border value
     cdef my_type neighborhood_max = border_value
@@ -34,6 +53,9 @@ cdef my_type get_neighborhood_max(
     cdef Py_ssize_t footprint_row_offset, footprint_col_offset
     cdef Py_ssize_t image_rows = image.shape[0]
     cdef Py_ssize_t image_cols = image.shape[1]
+
+    cdef Py_ssize_t footprint_center_row = footprint.shape[0] // 2
+    cdef Py_ssize_t footprint_center_col = footprint.shape[1] // 2
 
     for footprint_row_offset in range(-footprint_center_row, footprint_center_row + 1):
         for footprint_col_offset in range(
@@ -71,15 +93,37 @@ cdef uint8_t should_propagate(
     Py_ssize_t point_col,
     my_type point_value,
     uint8_t[:, ::1] footprint,
-    Py_ssize_t footprint_center_row,
-    Py_ssize_t footprint_center_col,
 ):
+    """Determine if a point should be propagated to its neighbors.
+
+    This implements the queue test during the raster scan/anti-scan.
+    If this function is true, the point's value may need to propagate
+    through the image.
+
+    The image and mask must be of the same type and shape. The footprint
+    must have an odd number of rows and columns, and is anchored at the
+    center point. In the fast-hybrid-reconstruct algorithm, the
+    footprint is the raster footprint but excluding the center point.
+
+    Args:
+        image (my_type[][]): the image to scan
+        mask (my_type[][]): the mask to scan
+        point_row (Py_ssize_t): the row of the point to scan
+        point_col (Py_ssize_t): the column of the point to scan
+        point_value (my_type): the value of the point to scan
+        footprint (uint8_t[][]): the neighborhood footprint
+
+    Returns:
+        uint8_t: 1 if the point should be propagated, 0 otherwise.
+    """
     cdef Py_ssize_t footprint_row_offset, footprint_col_offset
     cdef Py_ssize_t neighbor_row, neighbor_col
     cdef my_type neighbor_value
     cdef Py_ssize_t footprint_x, footprint_y
     cdef Py_ssize_t image_rows = image.shape[0]
     cdef Py_ssize_t image_cols = image.shape[1]
+    cdef Py_ssize_t footprint_center_row = footprint.shape[0] // 2
+    cdef Py_ssize_t footprint_center_col = footprint.shape[1] // 2
 
     for footprint_row_offset in range(-footprint_center_row, footprint_center_row + 1):
         for footprint_col_offset in range(
@@ -115,7 +159,7 @@ cdef uint8_t should_propagate(
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def fast_hybrid_loop(
+def fast_hybrid_raster_scans(
     my_type[:, ::1] marker,
     my_type[:, ::1] mask,
     uint8_t[:, ::1] footprint_raster_before,
@@ -124,6 +168,27 @@ def fast_hybrid_loop(
     my_type border_value,
     queue,
 ):
+    """Apply a maximum filter in raster order, then in reverse-raster order.
+
+    This implements the raster scan and anti-scan portions of the fast
+    hybrid reconstruct algorithm. The scan applies a maximum filter to
+    each point, using the pixels before or after the point depending on
+    the scan order.
+
+    After being scanned in both orders, each point is tested for further
+    propagation. If so, the point is added to the provided queue.
+
+    Note that this modifies the marker image in place.
+
+    Args:
+        marker (my_type[][]): the image to scan
+        mask (my_type[][]): the mask to scan
+        footprint_raster_before (uint8_t[][]): the raster footprint before the center point
+        footprint_raster_after (uint8_t[][]): the raster footprint after the center point
+        footprint_propagation_test (uint8_t[][]): the raster footprint after the center point, excluding the center point
+        border_value (my_type): the value to use for out-of-bound points
+        queue (deque): the queue of points to process
+    """
     cdef Py_ssize_t row, col
     cdef Py_ssize_t marker_rows, marker_cols
     cdef Py_ssize_t footprint_rows, footprint_cols
@@ -153,16 +218,14 @@ def fast_hybrid_loop(
                 row,
                 col,
                 footprint_raster_before,
-                footprint_center_row,
-                footprint_center_col,
                 border_value,
             )
             marker[row, col] = min(neighborhood_max, point_mask)
 
     logging.debug("Raster scan time: %s", timeit.default_timer() - t)
-    t = timeit.default_timer()
 
     # Scan in reverse-raster order.
+    t = timeit.default_timer()
     for row in range(marker_rows - 1, -1, -1):
         for col in range(marker_cols - 1, -1, -1):
             # If we're already at the mask, skip the neighbor test.
@@ -173,8 +236,6 @@ def fast_hybrid_loop(
                     row,
                     col,
                     footprint_raster_after,
-                    footprint_center_row,
-                    footprint_center_col,
                     border_value,
                 )
                 point_mask = mask[row, col]
@@ -188,8 +249,6 @@ def fast_hybrid_loop(
                     col,
                     marker[row, col],
                     footprint_propagation_test,
-                    footprint_center_row,
-                    footprint_center_col,
             ):
                 queue.append((row, col))
 
@@ -200,18 +259,33 @@ def fast_hybrid_loop(
 @cython.wraparound(False)
 def process_queue(
    my_type[:, ::1] marker,
-   Py_ssize_t marker_rows,
-   Py_ssize_t marker_cols,
    my_type[:, ::1] mask,
    uint8_t[:, ::1] footprint,
-   Py_ssize_t footprint_center_row,
-   Py_ssize_t footprint_center_col,
    queue,
 ):
+    """Process the queue of pixels to propagate through a marker image.
+
+    This implements the queue phase of the fast-hybrid reconstruction
+    algorithm. During the raster scan phases, we identify pixels that
+    may need to propagate through the image. This phase processes
+    those queues, propagating the points further as necessary.
+
+    Note that this modifies the marker image in place.
+
+    Args:
+        marker (mytype[][]): the marker image to scan
+        mask (mytype[][]): the image mask (ceiling on image values)
+        footprint (uint8_t[][]): the neighborhood footprint
+        queue (deque): the queue of points to process
+    """
+    cdef Py_ssize_t marker_rows = marker.shape[0]
+    cdef Py_ssize_t marker_cols = marker.shape[1]
     cdef Py_ssize_t row, col
     cdef Py_ssize_t footprint_row_offset, footprint_col_offset
     cdef Py_ssize_t neighbor_row, neighbor_col
     cdef my_type neighbor_mask, neighbor_value, point_value
+    cdef Py_ssize_t footprint_center_row = footprint.shape[0] // 2
+    cdef Py_ssize_t footprint_center_col = footprint.shape[1] // 2
 
     # Process the queue of pixels that need to be updated.
     logging.debug("Queue size: %s", len(queue))
@@ -263,6 +337,39 @@ def process_queue(
 def fast_hybrid_reconstruct(
     my_type[:, ::1] marker, my_type[:, ::1] mask, radius = 2
 ):
+    """Perform grayscale reconstruction using the 'Fast-Hybrid' algorithm.
+
+    Functionally equivalent to scikit-image's grayreconstruct run in
+    dilation mode. That implementation uses the Downhill Filter algorithm.
+
+    This implements the fast-hybrid grayscale reconstruction algorithm as
+    described by Luc Vincent (1993). That paper isn't publicly available
+    but the algorithm is well-described in this paper:
+
+    Teodoro, George. (2012). A fast parallel implementation of queue-
+    based morphological reconstruction using gpus.
+
+    https://www.researchgate.net/publication/271444531_A_fast_parallel_implementation_of_queue-based_morphological_reconstruction_using_gpus
+
+    Note however that this implementation does not use the GPU
+    enhancements described in Teodoro's paper. Instead, it relies on Cython
+    to perform the sequential iterations very quickly. One of its main
+    advantages over the downhill filter implementation above is that it
+    avoids two sorts and accompanying memory allocations for several-
+    hundred-megabyte image files.
+
+    Note that this modifies the marker image in place.
+
+    TODO: accept a footprint not a radius. https://github.com/dchaley/deepcell-imaging/issues/30
+
+    Args:
+        marker (my_type[][]): the marker image
+        mask (my_type[][]): the mask image
+        radius (int): the radius of the neighborhood to use for reconstruction
+
+    Returns:
+        my_type[][]: the reconstructed marker image, modified in place
+    """
     cdef Py_ssize_t row, col
     cdef Py_ssize_t marker_rows, marker_cols
     cdef Py_ssize_t footprint_center_row, footprint_center_col
@@ -298,6 +405,7 @@ def fast_hybrid_reconstruct(
     ).reshape((footprint_rows, footprint_cols))
     footprint_raster_after = (footprint * ones_after).astype(np.uint8)
 
+    # The propagation test is N-(G), but without the center point.
     footprint_propagation_test = np.copy(footprint_raster_after)
     footprint_propagation_test[footprint_center_row, footprint_center_col] = False
 
@@ -307,9 +415,12 @@ def fast_hybrid_reconstruct(
     marker_rows = marker.shape[0]
     marker_cols = marker.shape[1]
 
+    # The propagation queue for after the raster scans.
     queue = deque()
 
-    fast_hybrid_loop(
+    # Apply the maximum filter in raster order, then in reverse-raster order.
+    # The center pixel is included in both of these tests.
+    fast_hybrid_raster_scans(
         marker,
         mask,
         footprint_raster_before,
@@ -319,17 +430,15 @@ def fast_hybrid_reconstruct(
         queue,
     )
 
+    # Propagate points as necessary.
+    # The center pixel is excluded from the neighborhood test.
+    footprint[footprint_center_row, footprint_center_col] = False
     process_queue(
         marker,
-        marker_rows,
-        marker_cols,
         mask,
         footprint,
-        footprint_center_row,
-        footprint_center_col,
         queue,
     )
 
-
+    # All done. Return marker (which was modified in place).
     return marker
-
