102eb343646fe6a638be22a1e38ce53e0a522f2d
David Haley <dchaley@gmail.com>
2024-03-14 14:51:11 -0700

    Clarify offset comment re: skimage

--- benchmarking/h_maxima/fast_reconstruct_wrapper.py
+++ benchmarking/h_maxima/fast_reconstruct_wrapper.py
@@ -35,13 +35,19 @@ def cython_reconstruct_wrapper(
     else:
         footprint = footprint.astype(np.uint8, copy=True)
 
+    # The existing skimage code is inconsistent in how it creates the offset.
+    # See the offset is None block: it creates offsets of different shapes.
+    # The default offset only has 1 dimension (a list of coordinates), but
+    # a provided offset must have the same dimensions as the footprint,
+    # shaped as a single element in each dimension.
     if offset is None:
         if not all([d % 2 == 1 for d in footprint.shape]):
             raise ValueError("Footprint dimensions must all be odd")
         offset = np.array([d // 2 for d in footprint.shape])
     else:
-        if offset.shape[0] != footprint.ndim:
-            raise ValueError("Offset length and footprint ndims must be equal.")
+        # Unlike skimage, make sure we have with 1 value per footprint dimension
+        if offset.shape != (footprint.ndim,):
+            raise ValueError("Offset must be of shape[footprint.ndim,].")
         if not all([(0 <= o < d) for o, d in zip(offset, footprint.shape)]):
             raise ValueError("Offset must be included inside footprint")
 
@@ -50,18 +56,6 @@ def cython_reconstruct_wrapper(
     else:
         image = image.astype(_supported_float_type(mask.dtype), copy=True)
 
-    # The existing skimage code is inconsistent in how it creates the offset.
-    # See the offset is None block: it creates offsets of different shapes.
-    # The default offset only has 1 dimension (a list of coordinates), but
-    # a provided offset must have the same dimensions as the footprint,
-    # shaped as a single element in each dimension.
-    # Somehow, it all still works.🤔
-    #
-    # But I couldn't figure out how to pass a C reference to the data.
-    # This creates a new bytes buffer, using the contiguous array data.
-    # Cython knows how to wire this into a pointer for the C function.
-    # When we support n-dimensions and need to figure out pointers for
-    # all types, remove this…
     offset = offset.astype(np.uint8, copy=True)
 
     fast_hybrid_reconstruct(
