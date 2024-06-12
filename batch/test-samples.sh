9c3a2daca3945d50b650e9458d125b74a5e2e587
David Haley <dchaley@gmail.com>
2024-06-12 15:52:02 -0700

    Script to submit 15 test Batch jobs

---
+++ batch/test-samples.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-1/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-2/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-3/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-4/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-5/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-6/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-7/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-8/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-9/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-10/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-11/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-12/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-13/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-14/input_channels.npz"
+./run-job.py --input_channels_path="gs://davids-genomics-data-public/cellular-segmentation/deep-cell/vanvalenlab-multiplex-20200810_tissue_dataset/mesmer-sample-15/input_channels.npz"
+
