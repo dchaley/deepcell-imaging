23456fb156856a4aa9e57e9fa2a9c069ac00b365
David Haley <dchaley@gmail.com>
2024-07-16 14:43:29 -0700

    Unify json for multistep & qupath

--- batch/run-qupath-job.py
+++ batch/run-qupath-job.py
@@ -6,6 +6,7 @@ import subprocess
 import tempfile
 import uuid
 
+from deepcell_imaging.gcp_batch_jobs import make_job_json
 from deepcell_imaging.numpy_utils import npz_headers
 
 CONTAINER_IMAGE = "us-central1-docker.pkg.dev/deepcell-on-batch/deepcell-benchmarking-us-central1/benchmarking:batch"
@@ -72,138 +73,15 @@ if len(input_file_contents) != 1:
     raise ValueError("Expected exactly one array in the input file")
 input_image_shape = input_file_contents[0][1]
 
-# A lot of stuff gets hardcoded in this json.
-# See the README for limitations.
-
-# Need to escape the curly braces in the JSON template
-base_json = """
-{{
-  "taskGroups": [
-    {{
-      "taskSpec": {{
-        "runnables": [
-          {{
-            "container": {{
-              "imageUri": "{container_image}",
-              "entrypoint": "python",
-              "commands": [
-                "scripts/preprocess.py",
-                "--image_uri={input_channels_path}",
-                "--benchmark_output_uri={job_intermediate_path}/preprocess_benchmark.json",
-                "--output_uri={job_intermediate_path}/preprocessed.npz.gz"
-              ]
-            }}
-          }},
-          {{
-            "container": {{
-              "imageUri": "{container_image}",
-              "entrypoint": "python",
-              "commands": [
-                "scripts/predict.py",
-                "--image_uri={job_intermediate_path}/preprocessed.npz.gz",
-                "--benchmark_output_uri={job_intermediate_path}/prediction_benchmark.json",
-                "--model_path={model_path}",
-                "--model_hash={model_hash}",
-                "--output_uri={job_intermediate_path}/raw_predictions.npz.gz"
-              ]
-            }}
-          }},
-          {{
-            "container": {{
-              "imageUri": "{container_image}",
-              "entrypoint": "python",
-              "commands": [
-                "scripts/postprocess.py",
-                "--raw_predictions_uri={job_intermediate_path}/raw_predictions.npz.gz",
-                "--input_rows={input_image_rows}",
-                "--input_cols={input_image_cols}",
-                "--benchmark_output_uri={job_intermediate_path}/postprocess_benchmark.json",
-                "--output_uri={job_intermediate_path}/predictions.npz.gz",
-                "--tiff_output_uri={tiff_output_uri}"
-              ]
-            }}
-          }},
-          {{
-            "container": {{
-              "imageUri": "{container_image}",
-              "entrypoint": "python",
-              "commands": [
-                "scripts/gather-benchmark.py",
-                "--preprocess_benchmarking_uri={job_intermediate_path}/preprocess_benchmark.json",
-                "--prediction_benchmarking_uri={job_intermediate_path}/prediction_benchmark.json",
-                "--postprocess_benchmarking_uri={job_intermediate_path}/postprocess_benchmark.json",
-                "--bigquery_benchmarking_table={bigquery_benchmarking_table}"
-              ]
-            }}
-          }},
-          {{
-            "container": {{
-              "imageUri": "{container_image}",
-              "entrypoint": "python",
-              "commands": [
-                "scripts/visualize.py",
-                "--image_uri={input_channels_path}",
-                "--predictions_uri={job_intermediate_path}/predictions.npz.gz",
-                "--visualized_input_uri={job_intermediate_path}/visualized_input.png",
-                "--visualized_predictions_uri={job_intermediate_path}/visualized_predictions.png"
-              ]
-            }}
-          }}
-        ],
-        "computeResource": {{
-          "memoryMib": 26000
-        }},
-        "maxRetryCount": 3,
-        "lifecyclePolicies": [
-          {{
-            "action": "RETRY_TASK",
-            "actionCondition": {{
-              "exitCodes": [50001]
-            }}
-          }}
-        ]
-      }},
-      "taskCount": 1,
-      "parallelism": 1
-    }}
-  ],
-  "allocationPolicy": {{
-    "instances": [
-      {{
-        "installGpuDrivers": true,
-        "policy": {{
-          "machineType": "n1-standard-8",
-          "provisioningModel": "SPOT",
-          "accelerators": [
-            {{
-              "type": "nvidia-tesla-t4",
-              "count": 1
-            }}
-          ]
-        }}
-      }}
-    ],
-    "location": {{
-      "allowedLocations": [
-        "regions/{region}"
-      ]
-    }}
-  }},
-  "logsPolicy": {{
-    "destination": "CLOUD_LOGGING"
-  }}
-}}
-"""
-
-job_json_str = base_json.format(
-    job_intermediate_path=job_intermediate_path,
-    tiff_output_uri=tiff_output_uri,
-    bigquery_benchmarking_table=bigquery_benchmarking_table,
+job_json_str = make_job_json(
+    region=REGION,
+    container_image=CONTAINER_IMAGE,
     model_path=args.model_path,
     model_hash=args.model_hash,
+    bigquery_benchmarking_table=bigquery_benchmarking_table,
     input_channels_path=input_channels_path,
-    container_image=CONTAINER_IMAGE,
-    region=REGION,
+    working_directory=job_intermediate_path,
+    tiff_output_uri=tiff_output_uri,
     input_image_rows=input_image_shape[0],
     input_image_cols=input_image_shape[1],
 )
