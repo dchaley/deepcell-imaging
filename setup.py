59f480f38dafd4dd767e7a9e3c56f1fa5520b46c
David Haley <dchaley@gmail.com>
2024-09-25 13:57:41 -0700

    Rename grayscale_reconstruction to image_processing

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("src/grayscale_reconstruction/fast_hybrid_impl.pyx"))
+setup(
+    ext_modules=cythonize("src/deepcell_imaging/image_processing/fast_hybrid_impl.pyx")
+)
